6c9aad53fa15f1a9b45a4e8c086a7f041ce48a53
buddini96 <buddinikl1996@gmail.com>
2023-12-17 17:02:18 +0530

    content updated

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var x = setInterval(function(){
 
     var days = Math.floor(distance / (1000 * 60 * 60 *24));
     var hours = Math.floor(distance % (1000 * 60 * 60 *24) / (1000 * 60 * 60));
-    var minutes = Math.floor(distance % (1000 * 60 * 60 *24) / (1000 * 60));
+    var minutes = Math.floor(distance / 1000 /60) % 60;
     var seconds = Math.floor(distance % (1000 * 60) / 1000);
 
     document.getElementById("days").innerHTML = days;
